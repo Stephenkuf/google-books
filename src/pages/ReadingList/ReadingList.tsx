159922af5af16fdfc8c252194a3e3a927ff67a8a
Adenuga Oluwatunmise <oluwatunmiseadenuga@gmail.com>
2022-08-17 05:34:23 +0100

    feat: fetch and display users reading list from local storage

--- src/pages/ReadingList/ReadingList.tsx
+++ src/pages/ReadingList/ReadingList.tsx
@@ -1,13 +1,23 @@
-import React from 'react'
 import { Container } from 'components/Container/Container'
 import { BooksGrid } from 'components/BooksGrid/BooksGrid'
 import { EmptyState } from 'components/EmptyState/EmptyState'
+import { useReadingList } from 'hooks/useReadingList/useReadingList'
 
 export const ReadingList = () => {
+  const { readingList, updateReadingList } = useReadingList();
+
   return (
     <Container>
-      <BooksGrid />
-      <EmptyState title="No Books Available in reading lists yet!" />
+      {readingList.length > 0 ? (
+        <>
+          <h1>
+            Books you plan to read &#128515;
+          </h1>
+          <BooksGrid books={readingList} readingList={readingList} updateReadingList={updateReadingList} />
+        </>
+      ) : (
+        <EmptyState title="No books in your reading list yet!!" />
+      )}
     </Container>
   )
 }
